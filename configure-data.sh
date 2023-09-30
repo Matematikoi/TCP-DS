e0edd13643e8247257d49aad11492d24dad627b5
Gabriel Lozano <golozanop@unal.edu.co>
2023-09-30 20:59:27 +0200

    adds usefull commands

--- configure-data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd tmp_data || exit 1 
-for file in * ; do
-    echo configuring $file
-    sed -i -e 's/searchstring/replacement/g' "$file"
-done
--- generate-data.sh
+++ generate-data.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 rm tmp_data/*
 cd tools || exit 1
-./dsdgen -scale 1 -dir ../tmp_data 
+echo ============= GENERATING DATA =============
+./dsdgen -scale $1 -dir ../tmp_data 
+cd ..
+echo ============= FORMATING DATA =============
+find tmp_data/ -type f -exec sed -ibak 's/.$//' {} ';'
