a2a60b67e114f4a9e1e5e01c58aa34eb13828760
Gabriel Lozano <golozanop@unal.edu.co>
2023-10-09 19:21:55 +0200

    adds python script beta

--- time_queries.py
+++ time_queries.py
@@ -1,20 +1,27 @@
-import pandas as pd 
 import subprocess
 import sys
 #docker cp tmp_queries mssqldev:/usr/config/tmp_queries 
 
 
-def run_query():
-    filename = 'cut_query_5_13.sql'
+def run_query(query_number):
+    filename = 'cut_query_0_{}.sql'.format(query_number)
     foo = subprocess.run(
         'docker exec mssqldev /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 2astazeY -d master -i /usr/config/tmp_queries/{}'
-        .format(filename).split()) 
-    print(foo)
+        .format(filename).split(), 
+        capture_output = True, 
+        text = True )
+    return 'Incorrect' not in foo.stdout
 
 
 def main() -> int:
-    run_query()
+    ok = 0
+    for i in range(100):
+        is_it_ok = run_query(i)
+        if not is_it_ok:
+            print('======================',i,'====================')
+    print (ok)
     return 0
+
 
 if __name__ == '__main__':
     sys.exit(main())  
