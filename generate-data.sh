e1c263f664641ad181332b8bda1f965834f11e83
Gabriel Lozano <golozanop@unal.edu.co>
2023-10-26 15:52:36 +0200

    adds time logs for etl

--- generate-data.sh
+++ generate-data.sh
@@ -1,23 +1,43 @@
 #!/bin/sh
+
+echo RESULTS:> etl_times.log
+echo ============= GENERATING DATA =============
+start_time=`date +%s`
 rm tmp_data/*
 cd tools || exit 1
-echo ============= GENERATING DATA =============
 ./dsdgen -scale $1 -dir ../tmp_data 
 cd ..
+sleep 2.1
+end_time=`date +%s`
+echo GENERATING DATA: `expr $end_time - $start_time` s. >> etl_times.log
 
 echo ============= COPYING DATA INTO DOCKER =============
+
 docker exec mssqldev rm /usr/config/tmp_data/*
 docker exec mssqldev rmdir /usr/config/tmp_data
 docker cp ./tmp_data mssqldev:/usr/config/tmp_data
 
+
+
 echo ============= FORMATING DATA =============
+start_time=`date +%s`
 docker exec mssqldev find tmp_data -name *dat -exec bash -c 'sed -i 's/.$//' "$1"' - {} \; 
+sleep 1
+end_time=`date +%s`
+echo FORMATING DATA: `expr $end_time - $start_time` s. >> etl_times.log
 
 echo ============= INSERTING DATA INTO SQL SERVER =============
+start_time=`date +%s`
 docker exec mssqldev /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 2astazeY -d master -i upload-data.sql
+sleep 3
+end_time=`date +%s`
+echo INSERTING DATA: `expr $end_time - $start_time` s. >> etl_times.log
 
 
 echo ============= CREATING INDEXES =============
+start_time=`date +%s`
 docker exec mssqldev /opt/mssql-tools/bin/sqlcmd -S localhost -U datawarehouse -P 7ellowEl7akey -d master -i create-index.sql
+end_time=`date +%s`
+echo CREATING INDEXES: `expr $end_time - $start_time` s. >> etl_times.log
 
 echo ============= DATA LOADED =============
